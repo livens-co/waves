3429cef1474bd6230a1e7e7ef6f12f2448a5b69d
dcvijetovic <dominik@mintandlime.eu>
2023-10-22 21:11:04 +0200

    first commit

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,20 +1,23 @@
-import { EmailTemplate } from '@/components/email-template';
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+import { EmailTemplate } from "@/components/email-template";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(email: string) {
-    try {
-        const data = await resend.emails.send({
-            from: 'noreply@livens.co',
-            to: email,
-            subject: 'Test Confirmation email',
-            react: EmailTemplate({ firstName: 'John' }) as React.ReactElement,
-        });
+export async function POST(
+  req: Request,
+  { params }: { params: { email: string } }
+) {
+  try {
+    const data = await resend.emails.send({
+      from: "noreply@livens.co",
+      to: params.email,
+      subject: "Test Confirmation email",
+      react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
+    });
 
-        return NextResponse.json(data);
-    } catch (error) {
-        return NextResponse.json({ error });
-    }
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
 }
