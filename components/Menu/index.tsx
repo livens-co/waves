bd9d872f852d7b203aeb90699c1111589cc6e90a
dcvijetovic <dominik@mintandlime.eu>
2023-10-27 20:41:16 +0200

    beta

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -20,39 +20,111 @@ const Menu = ({ openMenu, closeMenu }: MenuProps) => {
       className="menu"
       style={{
         right: openMenu ? "0" : "-100%",
-        transitionDelay: openMenu ? "0s" : "0.15s",
+        transitionDelay: openMenu ? "0s" : "0.1s",
       }}
     >
-      <button onClick={closeMenu} className="closeButton">
-        Close
-      </button>
       <div className="backgroundImage">
         <Image src="/assets/menuBackground.webp" alt="Waves & More" fill />
-        <div className="overlay"></div>
+        <div className="overlay" />
       </div>
-
+      <div className="logoMobile">
+          <Image
+            priority
+            src="/assets/logo.webp"
+            alt="Waves&More Logo"
+            width={200}
+            height={75}
+          />
+        </div>
       <div className="container">
         <div className="navigation">
           <div className="mainLinks">
-            <Link href="/" className="link" onClick={closeMenu}>
-              Home
-            </Link>
-            <Link href="/tours" className="link" onClick={closeMenu}>
+            <div className="menuLink">
+              <Link href="/" className="link" onClick={closeMenu}>
+                Home
+              </Link>
+            </div>
+            <div className="menuLink">
+              <Link href="/tours" className="link" onClick={closeMenu}>
               Seafaris Tours
-            </Link>
-            <Link href="/fleet-and-crew" className="link" onClick={closeMenu}>
+              </Link>
+            </div>
+            <div className="menuLink">
+              <Link href="/fleet-and-crew" className="link" onClick={closeMenu}>
               Fleet & Crew
-            </Link>
+              </Link>
+            </div>
           </div>
-          <Link href="/booking" className="menuBookNow" onClick={closeMenu}>
-            Book now
-          </Link>
+          <div className="menuBookNow">
+          <Link href="/booking"  onClick={closeMenu}>
+              Book now
+              </Link>
+          </div>
+          <div className="mobileLinks">
+            <div className="menuLink">
+              <Link
+                href="/"
+                className="link"
+                onClick={closeMenu}
+                style={{
+                  top: openMenu ? "0" : "100px",
+                  transitionDelay: openMenu ? "0.7s" : "0s",
+                }}
+              >
+                Home
+              </Link>
+              <div className="menuLinkWrapper" />
+            </div>
+            <div className="menuLink">
+              <Link
+                href="/tours"
+                className="link"
+                onClick={closeMenu}
+                style={{
+                  top: openMenu ? "0" : "100px",
+                  transitionDelay: openMenu ? "0.8s" : "0s",
+                }}
+              >
+                Seafaris Tours
+              </Link>
+              <div className="menuLinkWrapper" />
+            </div>
+            <div className="menuLink">
+              <Link
+                href="/fleet-and-crew"
+                className="link"
+                onClick={closeMenu}
+                style={{
+                  top: openMenu ? "0" : "100px",
+                  transitionDelay: openMenu ? "0.9s" : "0s",
+                }}
+              >
+                Fleet & Crew
+              </Link>
+              <div className="menuLinkWrapper" />
+            </div>
+            <div className="menuLink">
+              <Link
+                href="/booking"
+                className="link "
+                onClick={closeMenu}
+                style={{
+                  top: openMenu ? "0" : "100px",
+                  transitionDelay: openMenu ? "1s" : "0s",
+                }}
+              >
+                Book now
+              </Link>
+              <div className="menuLinkWrapper" />
+            </div>
+          </div>
         </div>
         <div className="social">
           <a href="https://www.instagram.com/waves_more/" target="_blank">
             <InstagramIcon />
             Instagram
           </a>
+
           <a href="https://www.tiktok.com/@wavesnmore" target="_blank">
             Tik Tok
           </a>
@@ -63,10 +135,6 @@ const Menu = ({ openMenu, closeMenu }: MenuProps) => {
             <FacebookIcon />
             Facebook
           </a>
-          {/* <a href="/">
-            <LinkedInIcon />
-            LinkedIn
-          </a> */}
         </div>
       </div>
       <div className="container">
