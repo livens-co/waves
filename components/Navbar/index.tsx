e4e49dde7bfdf0fbd3fbc444e1bfa00d5455fc3e
dcvijetovic <dominik@mintandlime.eu>
2023-10-27 12:41:59 +0200

    beta

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -11,10 +11,18 @@ const Navbar = () => {
     <>
       <nav>
         <div className="menuButton" onClick={() => setOpenMenu(!openMenu)}>
-          <div className="dot"></div>
-          <div className="dot"></div>
-          <div className="dot"></div>
+         <div className="line" style={{ width: '70%' }}/>
+              <div className="line" style={{ width: '85%' }}/>
+              <div className="line" style={{ width: '100%' }}/>
+          
         </div>
+        {/* <div className="circle">
+            <div className="menu-btn" onClick={() => setOpenMenu(!openMenu)}>
+              <div className="line" style={{ width: '70%' }}></div>
+              <div className="line" style={{ width: '85%' }}></div>
+              <div className="line" style={{ width: '100%' }}></div>
+            </div>
+          </div> */}
       </nav>
 
       <Menu openMenu={openMenu} closeMenu={() => setOpenMenu(!openMenu)} />
